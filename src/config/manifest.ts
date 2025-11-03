6d0ed025dfc5d8ad1c4124894d33a4f23cc6269c
nlyrthiia <nlyrthiia@outlook.com>
2025-11-03 12:35:09 +0800

    build

--- src/config/manifest.ts
+++ src/config/manifest.ts
@@ -18,8 +18,7 @@ const manifests = {
 const deployType = import.meta.env.VITE_PUBLIC_DEPLOY_TYPE as string;
 
 // Export the appropriate manifest with a fallback
-export const manifest = deployType in manifests
-  ? manifests[deployType as DeployType]
-  : sepolia; // Default to sepolia for production
+export const manifest =
+  deployType in manifests ? manifests[deployType as DeployType] : sepolia; // Default to sepolia for production
 
 export type Manifest = typeof manifest;
